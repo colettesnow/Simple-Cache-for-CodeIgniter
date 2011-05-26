03cd19999e287c22e2c6e67a5a2ed8b25d77f312
Colette Snow <colette.a.snow@gmail.com>
2011-05-26 21:45:25 +1000

    Commented the code!

--- Simple_cache.php
+++ Simple_cache.php
@@ -21,33 +21,41 @@
 
 */
 
-
 class Simple_cache {
+ 
+	/**
+	 * @var int containing the number of seconds that a cached item will be considered current
+	 */
+	public $expire_after = 300;
 
-	public $expire_after;
-
-	function Simple_cache()
-	{
-		if ($this->expire_after == '')
-		{
-			 $this->expire_after = 300;
-		}
-	}
-
+	/**
+	 * Caches an item which can be retrieved by key
+	 *	
+	 * @param string $key identitifer to retrieve the data later
+	 * @param mixed $value to be cached
+	 */
 	function cache_item($key, $value)
 	{
-	
+		// hashing the key in order to ensure that the item is stored with an appropriate file name in the file system.	
 		$key = sha1($key);
 		
+		// serialises the contents so that they can be stored in plain text
 		$value = serialize($value);
 		
 		file_put_contents(BASEPATH.'cache/'.$key.'.cache', $value);		
 	
 	}
 	
+	/**
+	 * Check's whether an item is cached or not
+	 * 
+	 * @param string $key containing the identifier of the cached item
+	 * @return bool whether the item is currently cached or not
+	 */ 
 	function is_cached($key)
 	{
 		$key = sha1($key);
+		// checks if the cached item exists and that it has not expired. 
 		if (file_exists(BASEPATH.'cache/'.$key.'.cache') && (filectime(BASEPATH.'cache/'.$key.'.php')+$this->expire_after) >= time())
 		{			
 			return true;
@@ -56,6 +64,12 @@ class Simple_cache {
 		}		
 	}
 	
+	/**
+	 * Retrieve's the cached item
+	 *
+	 * @param string $key containing the identifier of the item to retrieve
+	 * @return mixed the cached item or items
+	 */
 	function get_item($key)
 	{
 		$key = sha1($key);
@@ -65,10 +79,14 @@ class Simple_cache {
 		return $items;
 	}
 	
+	/**
+	 * Delete's the cached item
+	 * 
+	 * @param string $key containing the identifier of the item to delete.
+	*/
 	function delete_item($key)
 	{
 		unlink(BASEPATH.'cache/'.sha1($key).'.cache');
 	}
 
 }
-
